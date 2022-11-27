c5fd0fa3a9ab4a2d1317a881d4b7e12386931956
kerimhejsalah <k.hejsalah@gmail.com>
2022-11-27 21:14:58 +0100

    testtt

--- src/app/matdialogauth/matdialogauth.component.ts
+++ src/app/matdialogauth/matdialogauth.component.ts
@@ -1,16 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
-import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
 @Component({
   selector: 'app-matdialogauth',
   templateUrl: './matdialogauth.component.html',
   styleUrls: ['./matdialogauth.component.css']
 })
 export class MatdialogauthComponent implements OnInit {
+  form!: FormGroup;
+  loading = false;
+  submitted = false;
 
-  constructor() { }
-
+  constructor(
+      private formBuilder: FormBuilder,
+      private route: ActivatedRoute,
+      private router: Router,
+  ) { }
   ngOnInit(): void {
-  }
+    this.form = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+  });
 
+  }
+  get f() { return this.form.controls; }
+
+    onSubmit(): void {} 
 }
